fdf3b94d9586679e12ac013f096c35bd22d8f41c
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-09-19 23:13:18 -0500

    Cleaning up imports

--- test/PrivacyProxy.t.sol
+++ test/PrivacyProxy.t.sol
@@ -1,26 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-import "forge-std/Test.sol" ;
+import "forge-std/Test.sol";
 import "src/PrivacyProxy.sol";
 import { Currency, CurrencyLibrary } from "@uniswap/v4-core/contracts/types/Currency.sol";
-import {Tornado, IVerifier} from "lib/tornado-core/contracts/Tornado.sol";
-import {IHasher} from "lib/tornado-core/contracts/MerkleTreeWithHistory.sol";
+import { Tornado, IVerifier } from "lib/tornado-core/contracts/Tornado.sol";
+import { IHasher } from "lib/tornado-core/contracts/MerkleTreeWithHistory.sol";
 import { IPoolManager, PoolKey } from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
-import { Currency, CurrencyLibrary } from "@uniswap/v4-core/contracts/types/Currency.sol";
 import { IERC20Minimal } from "@uniswap/v4-core/contracts/interfaces/external/IERC20Minimal.sol";
-import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
-import {Tornado, IVerifier} from "lib/tornado-core/contracts/Tornado.sol";
-import {IHasher} from "lib/tornado-core/contracts/MerkleTreeWithHistory.sol";
-import {HookTest} from "./utils/HookTest.sol";
-import {GasSnapshot} from "forge-gas-snapshot/GasSnapshot.sol";
-import {IHooks} from "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
-import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
-import {TickMath} from "@uniswap/v4-core/contracts/libraries/TickMath.sol";
-import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
-import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
-import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
-import {Deployers} from "@uniswap/v4-core/test/foundry-tests/utils/Deployers.sol";
-import {PrivacyHook} from "src/PrivacyHook.sol";
+import { HookTest } from "./utils/HookTest.sol";
+import { GasSnapshot } from "forge-gas-snapshot/GasSnapshot.sol";
+import { IHooks } from "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
+import { PoolId, PoolIdLibrary } from "@uniswap/v4-core/contracts/types/PoolId.sol";
+import { Deployers } from "@uniswap/v4-core/test/foundry-tests/utils/Deployers.sol";
+import { PrivacyHook } from "src/PrivacyHook.sol";
 
 contract PrivacyProxyTest is HookTest, Deployers, GasSnapshot {
     PrivacyProxy public proxy;
@@ -52,7 +44,8 @@ contract PrivacyProxyTest is HookTest, Deployers, GasSnapshot {
     }
 
     function testDeposit() public {
-        bytes32 commitment = "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef";
+        //todo(ajackson): Retrieve commitment generation from tornado cash test
+        bytes32 commitment;
         proxy.deposit(commitment, token0, token1, proxy.denomination, proxy.denomination2);
     }
 
